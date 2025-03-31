f72d00c26e1a63a69b8af5a3006128b06b48e3a1
Sarvesh <atsarveshyadav@gmail.com>
2025-03-31 23:07:43 +0530

    Add dynamic year to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,8 @@ export default function Footer(){
   return (
     <footer className="bg-white h-20 mt-5 flex items-center justify-center border border-b-0 border-l-0 border-r-0">
       <div>
-        <p className="text-xs text-black">Copyright 2023 E-Signature All Rights Reserved</p>
+        <p className="text-xs text-black">
+        © {new Date().getFullYear()} SignEase</p>
       </div>
     </footer>
   );
